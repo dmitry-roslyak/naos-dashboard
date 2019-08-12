5b275542cd22353272ea33344f6fafe5fccc4155
Dmitry Roslyak <dmitri.roslyak@gmail.com>
2019-08-12 11:40:23 +0300

    sequelize config update

--- dist/core/config-init.js
+++ dist/core/config-init.js
@@ -1,14 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const { NAOS_API_TOKEN, NAOS_URL, DATABASE_URL, isUserNamesVisible, sequelizeConfig } = require("../../config.json");
-!process.env.NAOS_API_TOKEN && NAOS_API_TOKEN && (process.env.NAOS_API_TOKEN = NAOS_API_TOKEN);
-!process.env.NAOS_URL && NAOS_URL && (process.env.NAOS_URL = NAOS_URL);
-!process.env.DATABASE_URL && DATABASE_URL && (process.env.DATABASE_URL = DATABASE_URL);
-!process.env.isUserNamesVisible && isUserNamesVisible && (process.env.isUserNamesVisible = isUserNamesVisible);
-const sequelize = process.env.DATABASE_URL ?
-    new sequelize_1.Sequelize(process.env.DATABASE_URL) :
-    new sequelize_1.Sequelize(sequelizeConfig.database, sequelizeConfig.username, sequelizeConfig.password, sequelizeConfig.options);
+const sequelize = new sequelize_1.Sequelize(process.env.DATABASE_URL, {
+    "define": {
+        "underscored": true,
+        "timestamps": false
+    },
+    "logging": false
+});
 exports.sequelize = sequelize;
 exports.default = sequelize;
 //# sourceMappingURL=config-init.js.map
--- src/core/config-init.ts
+++ src/core/config-init.ts
@@ -1,21 +1,12 @@
 import { Sequelize } from "sequelize";
 
-const {
-    NAOS_API_TOKEN,
-    NAOS_URL,
-    DATABASE_URL,
-    isUserNamesVisible,
-    sequelizeConfig
-} = require("../../config.json");
-
-!process.env.NAOS_API_TOKEN && NAOS_API_TOKEN && (process.env.NAOS_API_TOKEN = NAOS_API_TOKEN);
-!process.env.NAOS_URL && NAOS_URL && (process.env.NAOS_URL = NAOS_URL);
-!process.env.DATABASE_URL && DATABASE_URL && (process.env.DATABASE_URL = DATABASE_URL);
-!process.env.isUserNamesVisible && isUserNamesVisible && (process.env.isUserNamesVisible = isUserNamesVisible);
-
-const sequelize = process.env.DATABASE_URL ?
-    new Sequelize(process.env.DATABASE_URL) :
-    new Sequelize(sequelizeConfig.database, sequelizeConfig.username, sequelizeConfig.password, sequelizeConfig.options);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    "define": {
+        "underscored": true,
+        "timestamps": false
+    },
+    "logging": false
+})
 
 export { sequelize }
 export default sequelize
